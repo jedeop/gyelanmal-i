a0e4ec4b5247aef9c872b850466d310b4476e982
jwp0116 <cong0116@naver.com>
2020-03-20 22:07:54 +0900

    split init() to multiple functions and fix bugs

--- src/index.js
+++ src/index.js
@@ -5,60 +5,78 @@ import "firebase/firestore";
 
 import db from './firestore';
 
-utils.log('성공적으로 설치되었습니다!');
+utils.log('다운로드 완료!');
 
-function init() {
-  let vc = Entry.variableContainer;
-  let perfix = vc.getVariableByName('계란말이'); // 계란말이 변수의 Prefix
-  if (!perfix) return utils.log('이 작품은 계란말이를 사용하지 않습니다!');
-  else perfix = perfix.getValue();
-  utils.log(`이 작품은 '${perfix}'(으)로 시작하는 변수를 계란말이 변수로 사용합니다.`);
+window.gyelan = { // 관련 글로벌 변수들
+  vars: [],
+  unsubscribe: null,
+  perfixVar: null,
+  perfix: undefined,
+}
 
+function getPerfix() {
+  const result = gyelan.perfixVar.getValue();
+  utils.log(`이 작품은 '${result}'(으)로 시작하는 변수를 계란말이 변수로 사용합니다.`);
+  return result;
+}
+function getGyelanVars(vc, perfix) { // 계란말이 변수들 가져오기
   const gyelanVars = [
     ...vc.variables_
       .filter((d) => d.name_.startsWith(perfix)),
     ...vc.lists_
       .filter((d) => d.name_.startsWith(perfix)),
   ]
-  utils.log('이 작품은 다음 변수/리스트들을 계란말이 변수로 사용하고 있어요: ', gyelanVars.map((d) => d.name_).join(', '))
+  utils.log('이 작품은 다음 변수/리스트들을 계란말이 변수로 사용하고 있어요: ', gyelanVars.map((d) => d.name_).join(', '));
+  return gyelanVars;
+}
+function subscribe(ref) {
+  return ref.onSnapshot((doc) => { // firebase 정보가 변경되었을 때 엔트리 변수 동기화.
+    var source = doc.metadata.hasPendingWrites ? "Local" : "Server";
+    const data = doc.data();
+    utils.log(source, "의 데이터가 다음과 같이 변경 됨: ", data);
+    for (const key in data) {
+      let vari = _.find(gyelan.vars, { id_: key });
+      if (!vari) {
+        projectRef.update({
+          [key]: firebase.firestore.FieldValue.delete()
+        });
+        continue;
+      }
+      if (vari.type == 'variable') vari.setValue(data[key], false);
+      else vari.setArray(data[key], false);
+    }
+  }, (error) => {
+    utils.error('스냅샷 리스너를 연결하던 중 에러가 발생함.', error)
+  });
+}
+
+function init() {
+  let vc = Entry.variableContainer;
+  gyelan.perfixVar = vc.getVariableByName('계란말이'); // 계란말이 변수의 Prefix
+  if (!gyelan.perfixVar) return utils.log('이 작품은 계란말이를 사용하지 않습니다!');
+  else gyelan.perfix = getPerfix();
+
+  gyelan.vars = getGyelanVars(vc, gyelan.perfix);
   
   let projectRef = db.collection('project').doc(Entry.projectId);
   projectRef.set({}, { merge: true });
 
-  function subscribe(ref) {
-    return ref.onSnapshot((doc) => { // firebase 정보가 변경되었을 때 엔트리 변수 동기화.
-      var source = doc.metadata.hasPendingWrites ? "Local" : "Server";
-      const data = doc.data();
-      utils.log(source, "의 데이터가 다음과 같이 변경 됨: ", data);
-      for (const key in data) {
-        let vari = _.find(gyelanVars, { id_: key });
-        if(!vari) {
-          projectRef.update({
-            [key]: firebase.firestore.FieldValue.delete()
-          });
-          continue;
-        }
-        if (vari.type == 'variable') vari.setValue(data[key], false);
-        else vari.setArray(data[key], false);
-      }
-    }, (error) => {
-      utils.error('스냅샷 리스너를 연결하던 중 에러가 발생함.', error)
-    });
-  }
-
-  let unsubscribe = null;
   Entry.addEventListener('run', () => {
-    unsubscribe = subscribe(projectRef); // 작품 실행 시 동기화 켜기
+    if (document.location.pathname.startsWith('/ws/'))  {
+      gyelan.perfix = getPerfix();
+      gyelan.vars = getGyelanVars(vc, gyelan.perfix);
+    }
+    gyelan.unsubscribe = subscribe(projectRef); // 작품 실행 시 동기화 켜기
     utils.log('변수 동기화 시작');
   })
   Entry.addEventListener('stop', () => { // 작품 종료 시 동기화 끄기
-    unsubscribe();
+    gyelan.unsubscribe();
     utils.log('변수 동기화 종료');
   })
 
   // 변수 값 변경 시 자동으로 서버에 반영되도록 prototype 수정
   Entry.Variable.prototype.setValue = function (value, sync = true) {
-    if (sync && _.find(gyelanVars, { id_: this.id_ })) {
+    if (sync && _.find(gyelan.vars, { id_: this.id_ })) {
       projectRef.update({
         [this.id_]: value
       })
@@ -100,7 +118,7 @@ function init() {
         this.array_.push({
           data: value,
         });
-        if (_.find(gyelanVars, { id_: this.id_ })) {
+        if (_.find(gyelan.vars, { id_: this.id_ })) {
           projectRef.update({
             [this.id_]: this.array_
           })
@@ -133,7 +151,7 @@ function init() {
     proto.deleteValue = function (index) {
       if (!this.isRealTime_) {
         this.array_.splice(index - 1, 1);
-        if (_.find(gyelanVars, { id_: this.id_ })) {
+        if (_.find(gyelan.vars, { id_: this.id_ })) {
           projectRef.update({
             [this.id_]: this.array_
           })
@@ -164,7 +182,7 @@ function init() {
     proto.insertValue = function (index, data) {
       if (!this.isRealTime_) {
         this.array_.splice(index - 1, 0, { data });
-        if (_.find(gyelanVars, { id_: this.id_ })) {
+        if (_.find(gyelan.vars, { id_: this.id_ })) {
           projectRef.update({
             [this.id_]: this.array_
           })
@@ -195,7 +213,7 @@ function init() {
     proto.replaceValue = function (index, data) {
       if (!this.isRealTime_) {
         this.array_[index - 1].data = data;
-        if (_.find(gyelanVars, { id_: this.id_ })) {
+        if (_.find(gyelan.vars, { id_: this.id_ })) {
           projectRef.update({
             [this.id_]: this.array_
           })
@@ -224,6 +242,8 @@ function init() {
       }
     }
   }
+
+  utils.log('설치 완료!')
 }
 
 init();
